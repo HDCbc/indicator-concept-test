8c5b2b5a3c2ccc53e6586750baeb23c21f83f655
Daniel Egli <daniel@egli.ca>
2019-03-11 14:01:34 -0700

    looking for artifact

--- scripts/insertUpdate.sh
+++ scripts/insertUpdate.sh
@@ -1,7 +1,5 @@
-echo ${PRIVATE_TOKEN} > key
+ssh -o StrictHostKeyChecking=no gitlab@sandbox.hdcbc.ca << 'ENDSSH'
 
-ssh-add key
+docker exec db psql -v ON_ERROR_STOP=1 -U postgres -h localhost -c "INSERT INTO endpoint_update(id, effective_date, statement, signature) VALUES ((SELECT Max(id) + 1 FROM endpoint_update), CURRENT_TIMESTAMP, '', '');" central
 
-ssh gitlab@sandbox.hdcbc.ca
-
-docker exec db psql -v ON_ERROR_STOP=1 -U postgres -h localhost -c "INSERT INTO endpoint_update(id, effective_date, statement, signature) VALUES ((SELECT Max(id) + 1 FROM endpoint_update), CURRENT_TIMESTAMP, null, null);" central
+ENDSSH
