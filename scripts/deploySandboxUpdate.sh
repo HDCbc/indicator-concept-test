ea1db2fd29e32e0c5b9e1566f71adcfe6b382356
Daniel Egli <daniel@egli.ca>
2019-05-07 11:38:39 -0700

    Added new antipattern test

--- scripts/deploySandboxUpdate.sh
+++ scripts/deploySandboxUpdate.sh
@@ -5,12 +5,22 @@ scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null update-script.sq
 # SSH to the sandbox and issue the update via SQL
 # Echo's the INSERT syntax around the actual update script
 # Copy file into running db container
-# Execute script
+# Execute script to INSERT update to endpoint_update
 ssh -o StrictHostKeyChecking=no gitlab@sandbox.hdcbc.ca << 'ENDSSH'
 echo "SSH success to Sandbox"
-echo "$(echo "INSERT INTO endpoint_update(id, effective_date, statement, signature) VALUES ((SELECT Max(id) + 1 FROM endpoint_update), CURRENT_TIMESTAMP,'"; cat /hdc/scripts/update-script.sql; echo "', null)";)" > /hdc/scripts/update-script.sql
+echo "$(echo "INSERT INTO endpoint_update(id, effective_date, statement, signature) VALUES ((SELECT Max(id) + 1 FROM endpoint_update), CURRENT_TIMESTAMP,\$\$"; cat /hdc/scripts/update-script.sql; echo "\$\$, \$\$not set\$\$)";)" > /hdc/scripts/update-script.sql
 
-docker cp update-script.sql db:/update-script.sql
+echo "Copy script to db container and run"
+
+docker cp /hdc/scripts/update-script.sql db:/update-script.sql
 docker exec db psql -v ON_ERROR_STOP=1 -U postgres -f "/update-script.sql" central
 
+docker exec db psql -v ON_ERROR_STOP=1 -U postgres -c 'select * from purge_pre_tally()' central;
+
 ENDSSH
+
+
+
+# Run Purge Script to empty the endpoint_query indicator values
+# Run Tally
+# Run Merge Script to create uploads and upload data
---
+++ scripts/testSqlPatterns.sh
@@ -0,0 +1,4 @@
+# This file contains SQL anti pattern tests 
+
+Echo "Start anti pattern search"
+cat ../update-script.sql | grep 'ALTER OWNER'
